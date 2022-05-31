009551c132378dd7db71010eea236c213ae38939
Ross Cooper <ross.cooper@jump24.co.uk>
2022-05-31 14:03:35 +0100

    Added filtering

--- src/Loggers/RequestLogger.php
+++ src/Loggers/RequestLogger.php
@@ -20,10 +20,25 @@ class RequestLogger implements RequestLoggerContract
         $data = [
             'url' => $request->url(),
             'method' => $request->method(),
-            'headers' => $request->headers,
+            'headers' => $request->headers->all(),
             'body' => $request->all(),
             'user' => $request->getUser(),
         ];
+        $data = $this->filter($data);
         $this->logManager->debug(json_encode($data));
+    }
+
+    public function filter(array $data): array
+    {
+        return array_map(function ($value, $key) {
+            if (in_array(strtolower($key), ['password', 'token', 'authorization'])) {
+                return '*********';
+            }
+            if (is_array($value)) {
+                return $this->filter($value);
+            }
+
+            return $value;
+        }, $data);
     }
 }
